3f7d0c97d2fd95966df6f176baceac871383df59
sofi-rv <128938425+sofi-rv@users.noreply.github.com>
2023-08-03 02:52:22 +0000

    cambios

---
+++ src/js/component/trafficlights.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import "../../styles/trafficlights.css"
+
+const TrafficLights = () => {
+    const [color, setColor] = useState("red")
+    const [redOn, setRedOn] = useState(false)
+    const [yellowOn, setYellowOn] = useState(false)
+    const [greenOn, setGreenOn] = useState(false)
+
+    return (
+        <>
+            <div className="container">
+                <div className="row d-flex justify-content-center align-items-center">
+                    <div className="col d-flex justify-content-center flex-column align-items-center column">
+                        <div className={redOn == false ? "red" : "red on"} onClick={(e) => {
+                            setRedOn(!redOn)
+                            setYellowOn(false)
+                            setGreenOn(false)
+                        }}></div>
+                        <div className={!yellowOn ? "yellow" : "yellow on"} onClick={(e) => {
+                            setYellowOn(!yellowOn)
+                            setRedOn(false)
+                            setGreenOn(false)
+                        }}></div>
+                        <div className={!greenOn ? "green" : "green on"} onClick={(e) => {
+                            setGreenOn(!greenOn)
+                            setYellowOn(false)
+                            setRedOn(false)
+                        }}></div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+export default TrafficLights;
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import TrafficLights from "./component/trafficlights.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TrafficLights />, document.querySelector("#app"));
